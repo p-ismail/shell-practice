558b266c0cd979882fcac372cb1c257c0102824a
ismail-7337 <ismailp1140@gmail.com>
2026-08-02 11:14:45 +0530

    shell

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Please enter your username::"
-read USER_NAME
+read USER_NAME  #ttake the input and store it in read variable
 
 echo "user name is $USER_NAME"
 
 echo "Please enter your password::"
-read -s PASSWORD
+read -s PASSWORD #Here "-s" to help to hide the password
