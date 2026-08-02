5a964e7713d239e8d25c876eba427a039405effc
ismail-7337 <ismailp1140@gmail.com>
2026-08-02 12:54:20 +0530

    shell

--- 07-variables.sh
+++ 07-variables.sh
@@ -8,6 +8,7 @@ echo "Present directory: $PWD"
 echo "Who is running: $USER"
 echo "Home directory of current user: $HOME"
 echo "PID of this script: $$"
+echo "Exit code is : $?"
 sleep 100 &
 echo "PID of recently executed background process: $!"
 echo "All args passed to script: $*"
