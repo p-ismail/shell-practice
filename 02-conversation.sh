8d8b2af19f00862f0cdbea45d3325cf996f7c552
ismail-7337 <ismailp1140@gmail.com>
2026-08-02 10:43:58 +0530

    shell

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hello suresh.. How are you?"
+echo "Suresh:: I am doing good... what about you?"
+echo "Ramesh:: I am fine , waht are you learning"
+echo "Suresh:: I am learning shell"
