47ce79fd1d26d0fb0df54ef723073109b9c63179
ismail-7337 <ismailp1140@gmail.com>
2026-08-02 11:27:29 +0530

    shell

---
+++ 06-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+
+echo "script exceuted at $START_TIME"
+
+sleep 10
+
+END_TIME=$(date +%s)
+
+TOTAL_TIME=(($END_TIME-$START_TIME))
+
+echo "script exceuted in $TOTAL_TIME"
