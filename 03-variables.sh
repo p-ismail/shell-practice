56b82bdc4305807ac3634e03595ac490c8839e73
ismail-7337 <ismailp1140@gmail.com>
2026-08-02 11:03:23 +0530

    shell

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh #There is no space between value and =
+PERSON2=Suresh
+
+echo "$PERSON1:: Hello $PERSON2.. How are you?"
+echo "$PERSON2:: I am doing good... what about you?"
+echo "$PERSON1:: I am fine , waht are you learning"
+echo "$PERSON2:: I am learning shell"
