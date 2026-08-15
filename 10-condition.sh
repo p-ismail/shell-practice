1cfa30c1c70601ea5d99f9a06b63025ad510ef62
ismail-7337 <ismailp1140@gmail.com>
2026-08-15 12:25:15 +0530

    shell-roboshop

--- 10-condition.sh
+++ 10-condition.sh
@@ -3,17 +3,17 @@
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
-echo "run the script with the root user access"
-exit 1
+    echo "run the script with the root user access"
+    exit 1
 fi
 
-echo "installing nginx"
-dnf install nginx -y
+    echo "installing nginx"
+    dnf install nginx -y
 
 if [ $? -ne 0 ]; then
-echo "Installing nginx....Failure"
-exit 1
+    echo "Installing nginx....Failure"
+    exit 1
 else 
-echo "Installing nginx....Success"
+    echo "Installing nginx....Success"
 fi
 
